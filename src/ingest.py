dc7417fb8a8f78215e7ebe8941713408451ac2fc
ranggaprakoso0905 <ranggaprakoso@gmail.com>
2026-05-21 13:39:49 +0700

    Fixed bug

--- src/ingest.py
+++ src/ingest.py
@@ -6,7 +6,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
 
-from src.config import EMBEDDING_MODEL_NAME, VECTORSTORE_DIR
+from src.config import EMBEDDING_MODEL_NAME, VECTORSTORE_DIR, DOCS_DIR
 
 def build_vectorstore():
 
